4f497ba1e844db3940f8d0c1658fb38da5c8392f
Vaibhav Naswa <103403934+vnaswa@users.noreply.github.com>
2023-10-02 23:05:08 +0530

    Add files via upload

---
+++ Climbing the Learderboard.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    ranked = sorted(list(set(ranked)), reverse=True)
+    player = sorted(player, reverse=True)
+    
+    l=len(ranked)
+    j=0
+    
+    ans=[]
+    for i in range(len(player)):
+        while j<l and player[i]<ranked[j]:
+            j+=1
+        
+        ans.append(j+1)
+        
+    return ans[::-1]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
